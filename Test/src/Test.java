47a5bb25727c7e1c588d779512cd137f498ca464
Qixiong Liu <liuqixiong1991@gmail.com>
2018-03-27 01:46:33 -0700

    test

---
+++ Test/src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("1234");
+		sb.append("\t");
+		sb.append("3456");
+		sb.append("\t");
+//		String str = sb.substring(0, sb.length()-1);
+//		System.out.println(str);
+		System.out.println(sb.toString());
+	}
+}
